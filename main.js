2bbdf5fd4e8814804c5005631fee7d06521290a9
Ved Tiwari <79772110+ved-tiwari@users.noreply.github.com>
2024-03-15 02:10:40 -0400

    uploaded files

---
+++ main.js
@@ -0,0 +1,23 @@
+$("#cutu").click(function () {
+    alert("The paragraph was clicked.");
+});
+
+function cutu() {
+
+    $("#poem").slideDown();
+    $("#everyImage").slideUp();
+    $("#diff").slideUp();
+
+}
+
+function mine() {
+    $("#poem").slideUp();
+    $("#everyImage").slideDown();
+    $("#diff").slideUp();
+}
+
+function perf() {
+    $("#poem").slideUp();
+    $("#everyImage").slideUp();
+    $("#diff").slideDown();
+}
